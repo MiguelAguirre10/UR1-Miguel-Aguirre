47ca20b9cab96c1dc1d25e561af849e745efa398
maguirred <maguirred@ltu.edu>
2024-02-17 12:57:26 -0500

    Added multiple filters for HSV

--- UR1-Miguel-Aguirre/Form1.Designer.cs
+++ UR1-Miguel-Aguirre/Form1.Designer.cs
@@ -38,17 +38,43 @@
             textBox3 = new TextBox();
             textBox4 = new TextBox();
             textBox5 = new TextBox();
+            VideoPictureBox4 = new PictureBox();
+            VideoPictureBox3 = new PictureBox();
+            VideoPictureBox5 = new PictureBox();
+            VideoPictureBox6 = new PictureBox();
+            TrackBarMaxV = new TrackBar();
+            TrackBarMinV = new TrackBar();
+            TrackBarMaxS = new TrackBar();
+            TrackBarMinS = new TrackBar();
+            TrackBarMaxH = new TrackBar();
+            TrackBarMinH = new TrackBar();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
             ((System.ComponentModel.ISupportInitialize)VideoPictureBox).BeginInit();
             ((System.ComponentModel.ISupportInitialize)VideoPictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)TrackBarMax).BeginInit();
             ((System.ComponentModel.ISupportInitialize)TrackBarMin).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)VideoPictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)VideoPictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)VideoPictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)VideoPictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)TrackBarMaxV).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)TrackBarMinV).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)TrackBarMaxS).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)TrackBarMinS).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)TrackBarMaxH).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)TrackBarMinH).BeginInit();
             SuspendLayout();
             // 
             // StartStopBtn
             // 
-            StartStopBtn.Location = new Point(818, 760);
+            StartStopBtn.Location = new Point(1309, 753);
             StartStopBtn.Name = "StartStopBtn";
-            StartStopBtn.Size = new Size(170, 71);
+            StartStopBtn.Size = new Size(195, 90);
             StartStopBtn.TabIndex = 0;
             StartStopBtn.Text = "Start";
             StartStopBtn.UseVisualStyleBackColor = true;
@@ -56,7 +82,7 @@
             // 
             // VideoPictureBox
             // 
-            VideoPictureBox.Location = new Point(12, 12);
+            VideoPictureBox.Location = new Point(976, 12);
             VideoPictureBox.Name = "VideoPictureBox";
             VideoPictureBox.Size = new Size(874, 673);
             VideoPictureBox.TabIndex = 1;
@@ -64,7 +90,7 @@
             // 
             // VideoPictureBox2
             // 
-            VideoPictureBox2.Location = new Point(976, 12);
+            VideoPictureBox2.Location = new Point(1940, 12);
             VideoPictureBox2.Name = "VideoPictureBox2";
             VideoPictureBox2.Size = new Size(874, 673);
             VideoPictureBox2.TabIndex = 2;
@@ -72,10 +98,10 @@
             // 
             // TrackBarMax
             // 
-            TrackBarMax.Location = new Point(600, 837);
+            TrackBarMax.Location = new Point(2023, 736);
             TrackBarMax.Maximum = 255;
             TrackBarMax.Name = "TrackBarMax";
-            TrackBarMax.Size = new Size(604, 90);
+            TrackBarMax.Size = new Size(698, 90);
             TrackBarMax.TabIndex = 3;
             TrackBarMax.Value = 255;
             TrackBarMax.Visible = false;
@@ -83,17 +109,17 @@
             // 
             // TrackBarMin
             // 
-            TrackBarMin.Location = new Point(600, 933);
+            TrackBarMin.Location = new Point(2023, 832);
             TrackBarMin.Maximum = 255;
             TrackBarMin.Name = "TrackBarMin";
-            TrackBarMin.Size = new Size(604, 90);
+            TrackBarMin.Size = new Size(698, 90);
             TrackBarMin.TabIndex = 4;
             TrackBarMin.Visible = false;
             TrackBarMin.Scroll += TrackBarMin_Scroll;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(976, 691);
+            textBox1.Location = new Point(1940, 691);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(170, 39);
             textBox1.TabIndex = 5;
@@ -102,7 +128,7 @@
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(1152, 691);
+            textBox2.Location = new Point(2116, 691);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(170, 39);
             textBox2.TabIndex = 6;
@@ -111,7 +137,7 @@
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(1328, 691);
+            textBox3.Location = new Point(2292, 691);
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(170, 39);
             textBox3.TabIndex = 7;
@@ -120,7 +146,7 @@
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(1504, 691);
+            textBox4.Location = new Point(2468, 691);
             textBox4.Name = "textBox4";
             textBox4.Size = new Size(170, 39);
             textBox4.TabIndex = 8;
@@ -129,18 +155,189 @@
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(1680, 691);
+            textBox5.Location = new Point(2644, 691);
             textBox5.Name = "textBox5";
             textBox5.Size = new Size(170, 39);
             textBox5.TabIndex = 9;
             textBox5.TextAlign = HorizontalAlignment.Center;
             textBox5.Visible = false;
             // 
+            // VideoPictureBox4
+            // 
+            VideoPictureBox4.Location = new Point(976, 947);
+            VideoPictureBox4.Name = "VideoPictureBox4";
+            VideoPictureBox4.Size = new Size(874, 673);
+            VideoPictureBox4.TabIndex = 10;
+            VideoPictureBox4.TabStop = false;
+            // 
+            // VideoPictureBox3
+            // 
+            VideoPictureBox3.Location = new Point(12, 947);
+            VideoPictureBox3.Name = "VideoPictureBox3";
+            VideoPictureBox3.Size = new Size(874, 673);
+            VideoPictureBox3.TabIndex = 11;
+            VideoPictureBox3.TabStop = false;
+            // 
+            // VideoPictureBox5
+            // 
+            VideoPictureBox5.Location = new Point(1940, 947);
+            VideoPictureBox5.Name = "VideoPictureBox5";
+            VideoPictureBox5.Size = new Size(874, 673);
+            VideoPictureBox5.TabIndex = 12;
+            VideoPictureBox5.TabStop = false;
+            // 
+            // VideoPictureBox6
+            // 
+            VideoPictureBox6.Location = new Point(2908, 947);
+            VideoPictureBox6.Name = "VideoPictureBox6";
+            VideoPictureBox6.Size = new Size(874, 673);
+            VideoPictureBox6.TabIndex = 13;
+            VideoPictureBox6.TabStop = false;
+            // 
+            // TrackBarMaxV
+            // 
+            TrackBarMaxV.Location = new Point(2023, 1626);
+            TrackBarMaxV.Maximum = 255;
+            TrackBarMaxV.Name = "TrackBarMaxV";
+            TrackBarMaxV.Size = new Size(698, 90);
+            TrackBarMaxV.TabIndex = 14;
+            TrackBarMaxV.Value = 255;
+            TrackBarMaxV.Visible = false;
+            TrackBarMaxV.Scroll += TrackBarMaxV_Scroll;
+            // 
+            // TrackBarMinV
+            // 
+            TrackBarMinV.Location = new Point(2023, 1722);
+            TrackBarMinV.Maximum = 255;
+            TrackBarMinV.Name = "TrackBarMinV";
+            TrackBarMinV.Size = new Size(698, 90);
+            TrackBarMinV.TabIndex = 15;
+            TrackBarMinV.Visible = false;
+            TrackBarMinV.Scroll += TrackBarMinV_Scroll;
+            // 
+            // TrackBarMaxS
+            // 
+            TrackBarMaxS.Location = new Point(1071, 1626);
+            TrackBarMaxS.Maximum = 255;
+            TrackBarMaxS.Name = "TrackBarMaxS";
+            TrackBarMaxS.Size = new Size(698, 90);
+            TrackBarMaxS.TabIndex = 16;
+            TrackBarMaxS.Value = 255;
+            TrackBarMaxS.Visible = false;
+            TrackBarMaxS.Scroll += TrackBarMaxS_Scroll;
+            // 
+            // TrackBarMinS
+            // 
+            TrackBarMinS.Location = new Point(1071, 1722);
+            TrackBarMinS.Maximum = 255;
+            TrackBarMinS.Name = "TrackBarMinS";
+            TrackBarMinS.Size = new Size(698, 90);
+            TrackBarMinS.TabIndex = 17;
+            TrackBarMinS.Visible = false;
+            TrackBarMinS.Scroll += TrackBarMinS_Scroll;
+            // 
+            // TrackBarMaxH
+            // 
+            TrackBarMaxH.Location = new Point(101, 1626);
+            TrackBarMaxH.Maximum = 255;
+            TrackBarMaxH.Name = "TrackBarMaxH";
+            TrackBarMaxH.Size = new Size(698, 90);
+            TrackBarMaxH.TabIndex = 18;
+            TrackBarMaxH.Value = 255;
+            TrackBarMaxH.Visible = false;
+            TrackBarMaxH.Scroll += TrackBarMaxH_Scroll;
+            // 
+            // TrackBarMinH
+            // 
+            TrackBarMinH.Location = new Point(101, 1722);
+            TrackBarMinH.Maximum = 255;
+            TrackBarMinH.Name = "TrackBarMinH";
+            TrackBarMinH.Size = new Size(698, 90);
+            TrackBarMinH.TabIndex = 19;
+            TrackBarMinH.Visible = false;
+            TrackBarMinH.Scroll += TrackBarMinH_Scroll;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(976, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 32);
+            label1.TabIndex = 20;
+            label1.Text = "Raw:";
+            label1.Visible = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(1940, 12);
+            label2.Name = "label2";
+            label2.Size = new Size(207, 32);
+            label2.TabIndex = 21;
+            label2.Text = "Lane Line (Binary):";
+            label2.Visible = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 947);
+            label3.Name = "label3";
+            label3.Size = new Size(36, 32);
+            label3.TabIndex = 22;
+            label3.Text = "H:";
+            label3.Visible = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(976, 947);
+            label4.Name = "label4";
+            label4.Size = new Size(32, 32);
+            label4.TabIndex = 23;
+            label4.Text = "S:";
+            label4.Visible = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(1940, 947);
+            label5.Name = "label5";
+            label5.Size = new Size(34, 32);
+            label5.TabIndex = 24;
+            label5.Text = "V:";
+            label5.Visible = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(2908, 947);
+            label6.Name = "label6";
+            label6.Size = new Size(176, 32);
+            label6.TabIndex = 25;
+            label6.Text = "Red Line (HSV):";
+            label6.Visible = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1862, 1035);
+            ClientSize = new Size(3794, 1844);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(TrackBarMinH);
+            Controls.Add(TrackBarMaxH);
+            Controls.Add(TrackBarMinS);
+            Controls.Add(TrackBarMaxS);
+            Controls.Add(TrackBarMinV);
+            Controls.Add(TrackBarMaxV);
+            Controls.Add(VideoPictureBox6);
+            Controls.Add(VideoPictureBox5);
+            Controls.Add(VideoPictureBox3);
+            Controls.Add(VideoPictureBox4);
             Controls.Add(textBox5);
             Controls.Add(textBox4);
             Controls.Add(textBox3);
@@ -158,6 +355,16 @@
             ((System.ComponentModel.ISupportInitialize)VideoPictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)TrackBarMax).EndInit();
             ((System.ComponentModel.ISupportInitialize)TrackBarMin).EndInit();
+            ((System.ComponentModel.ISupportInitialize)VideoPictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)VideoPictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)VideoPictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)VideoPictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)TrackBarMaxV).EndInit();
+            ((System.ComponentModel.ISupportInitialize)TrackBarMinV).EndInit();
+            ((System.ComponentModel.ISupportInitialize)TrackBarMaxS).EndInit();
+            ((System.ComponentModel.ISupportInitialize)TrackBarMinS).EndInit();
+            ((System.ComponentModel.ISupportInitialize)TrackBarMaxH).EndInit();
+            ((System.ComponentModel.ISupportInitialize)TrackBarMinH).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -174,5 +381,21 @@
         private TextBox textBox3;
         private TextBox textBox4;
         private TextBox textBox5;
+        private PictureBox VideoPictureBox4;
+        private PictureBox VideoPictureBox3;
+        private PictureBox VideoPictureBox5;
+        private PictureBox VideoPictureBox6;
+        private TrackBar TrackBarMaxV;
+        private TrackBar TrackBarMinV;
+        private TrackBar TrackBarMaxS;
+        private TrackBar TrackBarMinS;
+        private TrackBar TrackBarMaxH;
+        private TrackBar TrackBarMinH;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
     }
 }
--- UR1-Miguel-Aguirre/Form1.cs
+++ UR1-Miguel-Aguirre/Form1.cs
@@ -12,8 +12,8 @@ namespace UR1_Miguel_Aguirre
 
         bool mIsCapturing = false; // capturing state indicator
 
-        private int minTrackBarValue = 0;
-        private int maxTrackBarValue = 255;
+        private int minTrackBarValue = 0, minTrackBarValueH = 0, minTrackBarValueS = 0, minTrackBarValueV = 0;
+        private int maxTrackBarValue = 255, maxTrackBarValueH = 255, maxTrackBarValueS = 255, maxTrackBarValueV = 255;
 
         public Form1()
         {
@@ -44,6 +44,12 @@ namespace UR1_Miguel_Aguirre
                 StartStopBtn.Text = "Start"; // inform accordingly
                 TrackBarMax.Visible = false; // make the max track bar invisible
                 TrackBarMin.Visible = false; // make the min track bar invisible
+                TrackBarMaxH.Visible = false; // make the max track bar H invisible
+                TrackBarMinH.Visible = false; // make the min track bar H invisible
+                TrackBarMaxS.Visible = false; // make the max track bar S invisible
+                TrackBarMinS.Visible = false; // make the min track bar S invisible
+                TrackBarMaxV.Visible = false; // make the max track bar V invisible
+                TrackBarMinV.Visible = false; // make the min track bar V invisible
             }
             else
             {
@@ -54,11 +60,23 @@ namespace UR1_Miguel_Aguirre
                 StartStopBtn.Text = "Stop"; // inform accordingly
                 TrackBarMax.Visible = true; // make the max track bar visible
                 TrackBarMin.Visible = true; // make the min track bar visible
+                TrackBarMaxH.Visible = true; // make the max track bar H visible
+                TrackBarMinH.Visible = true; // make the min track bar H visible
+                TrackBarMaxS.Visible = true; // make the max track bar S visible
+                TrackBarMinS.Visible = true; // make the min track bar S visible
+                TrackBarMaxV.Visible = true; // make the max track bar V visible
+                TrackBarMinV.Visible = true; // make the min track bar V visible
                 textBox1.Visible = true; // make textBox1 visible
                 textBox2.Visible = true; // make textBox2 visible
                 textBox3.Visible = true; // make textBox3 visible
                 textBox4.Visible = true; // make textBox4 visible
                 textBox5.Visible = true; // make textBox5 visible
+                label1.Visible = true; // make label1 visible
+                label2.Visible = true; // make label2 visible
+                label3.Visible = true; // make label3 visible
+                label4.Visible = true; // make label4 visible
+                label5.Visible = true; // make label5 visible
+                label6.Visible = true; // make label6 visible
             }
         }
 
@@ -73,13 +91,38 @@ namespace UR1_Miguel_Aguirre
                 Size newSize = new Size(VideoPictureBox.Size.Width, newHeight);
                 CvInvoke.Resize(frame, frame, newSize);
                 CvInvoke.Flip(frame, frame, FlipType.Horizontal); // flip display frame horizontally
+ 
+                VideoPictureBox.Image = frame.ToBitmap(); // display current raw frame
 
-                VideoPictureBox.Image = frame.ToBitmap(); // display current frame
+                Mat hsvFrame = new Mat();
+                CvInvoke.CvtColor(frame, hsvFrame, Emgu.CV.CvEnum.ColorConversion.Bgr2Hsv);
+
+                Mat[] hsvChannels = hsvFrame.Split();
+
+                Mat hueFilter = new Mat();
+                CvInvoke.InRange(hsvChannels[0], new ScalarArray(minTrackBarValueH), new ScalarArray(maxTrackBarValueH), hueFilter);
+                
+                VideoPictureBox3.Image = hueFilter.ToBitmap(); // display current hue frame
+
+                Mat saturationFilter = new Mat();
+                CvInvoke.InRange(hsvChannels[1], new ScalarArray(minTrackBarValueS), new ScalarArray(maxTrackBarValueS), saturationFilter);
+                
+                VideoPictureBox4.Image = saturationFilter.ToBitmap(); // display current saturation frame
+
+                Mat valueFilter = new Mat();
+                CvInvoke.InRange(hsvChannels[2], new ScalarArray(minTrackBarValueV), new ScalarArray(maxTrackBarValueV), valueFilter);
+                
+                VideoPictureBox5.Image = valueFilter.ToBitmap(); // display current value frame
+
+                Mat mergedImage = new Mat();
+                CvInvoke.BitwiseAnd(hueFilter, saturationFilter, mergedImage);
+                CvInvoke.BitwiseAnd(mergedImage, valueFilter, mergedImage);
+                VideoPictureBox6.Image = mergedImage.ToBitmap(); // display current merged frame
 
                 CvInvoke.CvtColor(frame, frame, ColorConversion.Bgr2Gray); // changing the image to gray
                 CvInvoke.Threshold(frame, frame, minTrackBarValue, maxTrackBarValue, ThresholdType.Binary); // apply binary thresholding
-                
-                VideoPictureBox2.Image = frame.ToBitmap(); // display current frame
+
+                VideoPictureBox2.Image = frame.ToBitmap(); // display current binary frame
 
                 int columnWidth = frame.Width / 5; // divide the frame width into 5 sections to create 5 columns
                 Mat[] columns = new Mat[5]; // create an array to store the individual columns of the image
@@ -139,6 +182,66 @@ namespace UR1_Miguel_Aguirre
             }
 
             maxTrackBarValue = TrackBarMax.Value;
+        }
+
+        private void TrackBarMaxH_Scroll(object sender, EventArgs e)
+        {
+            if (TrackBarMinH.Value > TrackBarMaxH.Value)
+            {
+                TrackBarMinH.Value = TrackBarMaxH.Value;
+            }
+
+            maxTrackBarValueH = TrackBarMaxH.Value;
+        }
+
+        private void TrackBarMinH_Scroll(object sender, EventArgs e)
+        {
+            if (TrackBarMinH.Value > TrackBarMaxH.Value)
+            {
+                TrackBarMaxH.Value = TrackBarMinH.Value;
+            }
+
+            minTrackBarValueH = TrackBarMinH.Value;
+        }
+
+        private void TrackBarMaxS_Scroll(object sender, EventArgs e)
+        {
+            if (TrackBarMinS.Value > TrackBarMaxS.Value)
+            {
+                TrackBarMinS.Value = TrackBarMaxS.Value;
+            }
+
+            maxTrackBarValueS = TrackBarMaxS.Value;
+        }
+
+        private void TrackBarMinS_Scroll(object sender, EventArgs e)
+        {
+            if (TrackBarMinS.Value > TrackBarMaxS.Value)
+            {
+                TrackBarMaxS.Value = TrackBarMinS.Value;
+            }
+
+            minTrackBarValueS = TrackBarMinS.Value;
+        }
+
+        private void TrackBarMaxV_Scroll(object sender, EventArgs e)
+        {
+            if (TrackBarMinV.Value > TrackBarMaxV.Value)
+            {
+                TrackBarMinV.Value = TrackBarMaxV.Value;
+            }
+
+            maxTrackBarValueV = TrackBarMaxV.Value;
+        }
+
+        private void TrackBarMinV_Scroll(object sender, EventArgs e)
+        {
+            if (TrackBarMinV.Value > TrackBarMaxV.Value)
+            {
+                TrackBarMaxV.Value = TrackBarMinV.Value;
+            }
+
+            minTrackBarValueV = TrackBarMinV.Value;
         }
     }
 }
